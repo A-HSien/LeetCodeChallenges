027c76422d58605d02c830ae24bcfc8af5bf4700
A-Hsien <hsiaohsien0325@gmail.com>
2018-09-13 19:32:14 +0800

    201809131932

--- assert.js
+++ assert.js
@@ -1,4 +1,6 @@
 function assert(expected, result) {
+    if (expected !== null && typeof expected === 'object') expected = JSON.stringify(expected);
+    if (result !== null && typeof result === 'object') result = JSON.stringify(result);
     console.log(JSON.stringify(expected === result) + ', result: ' + result);
-    if (expected !== result) console.warn('expected: ' + expected);
+    if (expected !== result) console.warn(`expected: ${expected} , but your result: ${result}`);
 };
